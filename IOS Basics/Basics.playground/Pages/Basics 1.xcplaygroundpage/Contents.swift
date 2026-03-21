99f64e13d3080dbf533f3d526f39a6e9cae6d5ad
Harjot Singh <bhatiaharjot2000@gmail.com>
2026-03-21 23:55:45 +0530

    Update Contents.swift

--- IOS Basics/Basics.playground/Pages/Basics 1.xcplaygroundpage/Contents.swift
+++ IOS Basics/Basics.playground/Pages/Basics 1.xcplaygroundpage/Contents.swift
@@ -1,2 +1,43 @@
 import Foundation
 
+// print
+// datatypes
+//var vs  let
+// if else
+
+// print
+print ("Hello World")
+
+//----------------------------------------------------------------
+
+// variables
+var abc=1
+// now var is to tell its variable
+// now swift is type safe even if datatype is not given it manages
+var one:Character="s"
+var two:String="Hello"
+var three:Int=12
+var four:Double=3.232
+var five:Float=23.322 // less precise than double use double
+var six:Bool=false
+
+//----------------------------------------------------------------
+// var vs let
+var istrue=true
+istrue=false
+
+let isTrue=true
+//isTrue=false
+//Cannot assign to value: 'isTrue' is a 'let' constant
+// let is to keep val constant var can change value
+
+//----------------------------------------------------------------
+// if else
+var isFalse=true
+if isFalse==true{
+    print(11)
+}
+else{
+    print (22)
+}
+
