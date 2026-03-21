d0a7d386a1326732144b8011549caef3298b5a9b
Harjot Singh <bhatiaharjot2000@gmail.com>
2026-03-22 01:17:18 +0530

    Update Contents.swift

--- IOS Basics/Basics.playground/Pages/Basics 2.xcplaygroundpage/Contents.swift
+++ IOS Basics/Basics.playground/Pages/Basics 2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,46 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+// fucntions
+//functions calls another function
+//gaurd statements
+//calculated variables
 
-//: [Next](@next)
+//---------------------------------------------------------------
+
+
+// functions
+let one:String="Hello"
+func printing (){
+    print (one)
+}
+printing() // fuc calling
+
+// parameterised functions
+func add(no1:Int,no2:Int){
+    print (no1+no2)
+}
+add(no1:232,no2:11)
+// while calling func pass para like this with parameter name and : then write value
+
+// calling one function from another
+func function1(var1:Int,var2:Int){
+    print(var1+var2)
+}
+func function2(){
+    function1(var1: 32, var2: 12)
+}
+function2()
+
+
+//---------------------------------------------------------------
+//while calling func pass para like this with parameter name and : then write value
+// lets make it normal with one thing
+func simple(_ text:String){
+    print (text)
+}
+simple("hello normal")
+
+
+//---------------------------------------------------------------
+//gaurd statements
+
